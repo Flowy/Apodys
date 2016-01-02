e946329a5f92a85ab426091eea18187f1b5028f6
Lukas <flowyk@gmail.com>
2016-01-02 19:14:49 +0100

    maven run javafx

---
+++ src/main/java/com/flowyk/apodys/ui/UserInterface.java
@@ -0,0 +1,22 @@
+package com.flowyk.apodys.ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UserInterface extends javafx.application.Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
